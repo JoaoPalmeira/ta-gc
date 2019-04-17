4096fdbbda5a2696e59faf80a879aea778cd8f8c
JoãoPalmeira <joopalmeira@gmail.com>
2019-04-17 11:30:02 +0100

    WebODM results
    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 15173727,
+    "boundingBox": {
+        "lx": 554033.5,
+        "ly": 4505756.79,
+        "lz": -18.45,
+        "ux": 554449.48,
+        "uy": 4506172.77,
+        "uz": 397.5299999999814
+    },
+    "tightBoundingBox": {
+        "lx": 554033.5,
+        "ly": 4505756.79,
+        "lz": -18.45,
+        "ux": 554449.48,
+        "uy": 4506092.72,
+        "uz": 61.18
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.8819940090179445,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
